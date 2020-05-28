6a042cbe23c13f6542ff94a1740942b8f933d5a5
TheGrandestNothing <zhilyaev.slava@mail.ru>
2020-05-28 17:28:27 +0500

    Matrix added to git

---
+++ MATRIXLABS/MATRIXLABS/Matrix2D.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "MatrixBase.h"
+class Matrix2D :
+	public MatrixBase
+{
+public:
+	 int element(unsigned int i, unsigned int j) const override;
+	 int& element(unsigned int i, unsigned int j) override;
+
+	 Matrix2D() : MatrixBase(2) {}
+private:
+	int matrix[2][2];
+};
+
---
+++ MATRIXLABS/MATRIXLABS/MatrixBase.h
@@ -0,0 +1,24 @@
+#include <fstream>
+#include<string>
+#pragma once
+class MatrixBase
+{
+public:
+	MatrixBase() = delete;
+	unsigned int size() const
+	{
+		return m_size;
+	};
+
+	virtual int element(unsigned int i, unsigned int j) const = 0;
+	virtual int& element(unsigned int i, unsigned int j) = 0;
+	void operator*=(int iMult);
+	void operator+=(MatrixBase& iAdd);
+	friend std::ostream& operator<<(std::ostream& stream, const MatrixBase& iMatrix);
+
+protected:
+	MatrixBase(unsigned int iSize) : m_size(iSize) {}
+private:
+	const unsigned int m_size;
+};
+
